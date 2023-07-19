16e26466662d2670b44dbed290d99e633207467d
ashaik65 <34375439+ashaik65@users.noreply.github.com>
2023-07-19 06:31:06 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,10 +1,9 @@
 #! /bin/bash
 
 sudo apt update
-sudo apt install openjdk-11-jre -y
-curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee \
+sudo apt install openjdk-17-jre -y
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
   /usr/share/keyrings/jenkins-keyring.asc > /dev/null
-
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
